3fd95314558a8e875a9f0f8c46412d1e8d6bc93b
Tin-Trung Pham <tintrungpham1@gmail.com>
2025-01-10 12:00:14 -0800

    Bug Fixes - ready for submission

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ import shlex
 import json
 from datetime import datetime
 
+# Default File Name
 file_path = 'tasks.json'
 
-def writeData(data):
+'''
+Function for saving current data to JSON file.
+'''
+def writeData(data, nextId):
+    data['nextId'] = nextId
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
-def main():
-    data = {}
+
+'''
+Function in charge of handling the JSON file opening or
+creation if no file exists.
+'''
+def handleJson():
+    data = {
+        'nextId' : 0
+    }
     # JSON File Handling
     try:
         with open(file_path, 'r') as file:
@@ -18,18 +30,37 @@ def main():
     except FileNotFoundError:
         print(f"\nFile '{file_path}' not found. Creating new file.\n" )
         with open(file_path, 'w') as file:
-            json.dump({}, file, indent=4)
+            json.dump(data, file, indent=4)
             data = json.load(file)
 
     except json.JSONDecodeError:
         print('Error decdoing JSON.')
+
+    return data
+
+
+'''
+Main executing function of Task Tracker CLI. 
+
+Contains all logic and error handling.
+'''
+def main():
+    data = handleJson()
     
     # Task Tracker Logic
     run = True
     while run:
         text = shlex.split(input('task-cli '))
         size = len(text)
-        idNum = int(list(data.keys())[-1]) + 1
+
+        if size > 1:
+            id = text[1]
+
+        if 'nextId' in data:
+            nextId = data['nextId']
+            del data['nextId']
+        
+        
         current_time = datetime.now()
         formatted_time = current_time.strftime("%d-%m-%Y %H:%M")
 
@@ -38,77 +69,93 @@ def main():
         ############################
         if text[0] == 'add':
             if size == 2:
-                data[idNum] = {
+                data[str(nextId)] = {
                     'description' : text[1],
                     'status' : 'todo',
                     'createdAt' : formatted_time,
                     'updatedAt' : formatted_time,
                 }
                 
-                print('Task added successfully (ID: ', idNum, ')')
-                idNum += 1
+                print('Task added successfully (ID: ', nextId, ')')
+                nextId += 1
             
             else:
                 print('Please input data to add.')
 
         elif text[0] == 'update':
             if size == 3:
-                id = text[1]
-                data[id]['description'] = text[2]
-                data[id]['updatedAt'] = formatted_time
+                if id not in data:
+                    print('Please input existing ID')
+                else:
+                    data[id]['description'] = text[2]
+                    data[id]['updatedAt'] = formatted_time
             
 
 
         elif text[0] == 'delete':
             if size == 2:
-                id = text[1]
-                del data[id]
+                if id not in data:
+                    print('please input existing ID')
+                else:
+                    del data[id]
 
         ################
         # Task Marking #
         ################
         elif text[0] == 'mark-in-progress':
             if size == 2:
-                id = text[1]
-                data[id]['status'] = 'in-progress'
+                if id not in data:
+                    print('please input existing ID')
+                else:
+                    data[id]['status'] = 'in-progress'
 
         elif text[0] == 'mark-done':
             if size == 2:
-                id = text[1]
-                data[id]['status'] = 'done'
+                if id not in data:
+                    print('please input existing ID')
+                else:
+                    data[id]['status'] = 'done'
         
         ################
         # Task Listing #
         ################
         elif text[0] == 'list':
+            count = 0
             # List all tasks
             if size == 1:
-                for value in data.values():
-                    print(value['description']) 
+                for id, value in data.items():
+                    print(id, ' - ', value['description'])
+                    count += 1 
 
             # List all tasks that are done
             elif text[1] == 'done':
-                for value in data.values():
+                for id, value in data.items():
                     if value['status'] == 'done':
-                        print(value['description']) 
+                        print(id, ' - ', value['description']) 
+                        count += 1 
 
             # List all tasks that are not done
             elif text[1] == 'todo':
-                for value in data.values():
+                for id, value in data.items():
                     if value['status'] == 'todo':
-                        print(value['description']) 
+                        print(id, ' - ', value['description']) 
+                        count += 1 
 
             # List all tasks that are in progress
             elif text[1] == 'in-progress':
-                for value in data.values():
+                for id, value in data.items():
                     if value['status'] == 'in-progress':
-                        print(value['description']) 
+                        print(id, ' - ', value['description']) 
+                        count += 1
+            if count == 0:
+                print('There are no items in the specified list')
+                
 
         #################
         # Exit Handling #
         #################
         if text[0] == 'q' or text[0] == 'quit':
-            writeData(data)
+            writeData(data, nextId)
             break
 
     # Exit Message
